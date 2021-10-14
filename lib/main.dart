ecb6dc75fa508bcc472cbdee8232ea26724f42a3
docent28 <eaa67@mail.ru>
2021-10-14 08:48:43 +0300

    обновили

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,
-      body: Align(
-        alignment: Alignment.centerRight,
-        child: Text(
-          "Политехнический колледж городского хозяйства",
-          style: TextStyle(color: Colors.blue, fontSize: 40, shadows: [
-            Shadow(
-              color: Colors.green,
-              offset: Offset(-50, -50),
-              blurRadius: 10,
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.green,
+        appBar: AppBar(
+          title: const Text("MyFirst App"),
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Text(
+            "This is My HomePage",
+            style: TextStyle(
+              color: Colors.indigo,
+              fontSize: 40,
+              fontWeight: FontWeight.w900,
             ),
-            Shadow(
-              color: Colors.green,
-              offset: Offset(70, 70),
-              blurRadius: 15,
-            ),
-          ]),
-          textAlign: TextAlign.right,
+          ),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
