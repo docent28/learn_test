ab1a984bd9d84dd7be8a5a3e6b1e8d2529c7b25d
docent28 <eaa67@mail.ru>
2021-10-15 14:54:19 +0300

    Подготовка к Assets Image

--- lib/main.dart
+++ lib/main.dart
@@ -1,96 +1,23 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MyFirstApp(),
-  );
+  runApp(MyApp());
 }
 
-class MyFirstApp extends StatefulWidget {
-  const MyFirstApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyFirstApp> createState() => _MyFirstAppState();
-}
-
-class _MyFirstAppState extends State<MyFirstApp> {
-  late bool _loading;
-  late double _progressValue;
-
-  @override
-  void initState() {
-    _loading = false;
-    _progressValue = 0.0;
-    super.initState();
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.indigo,
         appBar: AppBar(
-          backgroundColor: Colors.indigoAccent,
-          centerTitle: true,
-          title: Text("My First App"),
-        ),
-        body: Center(
-          child: Container(
-            padding: EdgeInsets.all(20),
-            child: _loading
-                ? Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      Text(
-                        "${(_progressValue * 100).round()}%",
-                        style: TextStyle(
-                          fontSize: 50,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.white,
-                        ),
-                      ),
-                      LinearProgressIndicator(
-                        minHeight: 15,
-                        value: _progressValue,
-                      ),
-                    ],
-                  )
-                : Text(
-                    "Нажмите на кнопку справа внизу",
-                    style: TextStyle(
-                      fontSize: 50,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+          title: Text(
+            "Adding Assets",
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              _loading = !_loading;
-              _updateProgress();
-            });
-          },
-          child: Icon(Icons.cloud_download),
-        ),
+        body: null,
       ),
     );
-  }
-
-  void _updateProgress() {
-    const oneSec = const Duration(seconds: 1);
-    Timer.periodic(oneSec, (Timer t) {
-      setState(() {
-        _progressValue += 0.1;
-        if (_progressValue.toStringAsFixed(1) == "1.0") {
-          _loading = false;
-          t.cancel();
-          _progressValue = 0.0;
-          return;
-        }
-      });
-    });
   }
 }
