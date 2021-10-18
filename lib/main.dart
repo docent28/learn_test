631fb3dd099eb564addde8a3b67f0de459d1d33e
docent28 <eaa67@mail.ru>
2021-10-18 16:48:20 +0300

    Начали делать Counter

--- lib/main.dart
+++ lib/main.dart
@@ -8,89 +8,12 @@ void main() {
   );
 }
 
-class MyFirstApp extends StatefulWidget {
+class MyFirstApp extends StatelessWidget {
   const MyFirstApp({Key? key}) : super(key: key);
 
   @override
-  State<MyFirstApp> createState() => _MyFirstAppState();
-}
-
-class _MyFirstAppState extends State<MyFirstApp> {
-  late bool _loading;
-  late double _progressValue;
-
-  @override
-  void initState() {
-    _loading = false;
-    _progressValue = 0.0;
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.indigo,
-        appBar: AppBar(
-          backgroundColor: Colors.indigoAccent,
-          centerTitle: true,
-          title: Text("My First App"),
-        ),
-        body: Center(
-          child: Container(
-            padding: EdgeInsets.all(20),
-            child: _loading
-                ? Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      Text(
-                        "${(_progressValue * 100).round()}%",
-                        style: TextStyle(
-                          fontSize: 45,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.white,
-                        ),
-                      ),
-                      LinearProgressIndicator(
-                        minHeight: 15,
-                        value: _progressValue,
-                      ),
-                    ],
-                  )
-                : Text(
-                    "Нажмите на кнопку справа внизу",
-                    style: TextStyle(
-                      fontSize: 50,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              _loading = !_loading;
-              _updateProgress();
-            });
-          },
-          child: Icon(Icons.cloud_download),
-        ),
-      ),
-    );
-  }
-
-  void _updateProgress() {
-    const oneSec = const Duration(seconds: 1);
-    Timer.periodic(oneSec, (Timer t) {
-      setState(() {
-        _progressValue += 0.1;
-        if (_progressValue.toStringAsFixed(1) == "1.0") {
-          _loading = false;
-          t.cancel();
-          _progressValue = 0.0;
-          return;
-        }
-      });
-    });
+    return Container();
   }
 }
+
