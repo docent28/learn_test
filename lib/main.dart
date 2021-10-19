d049237e51ee96b999239ffa816f2be7963edd10
docent28 <eaa67@mail.ru>
2021-10-19 13:27:33 +0300

    Counter работает

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +11,89 @@ class MyFirstApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("Counter"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              "Нажмите + для увеличения",
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(height: 25),
+            ChangingValue(),
+            SizedBox(height: 25),
+            Text(
+              "Нажмите - для уменьшения",
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
+class ChangingValue extends StatefulWidget {
+  const ChangingValue({Key? key}) : super(key: key);
+
+  @override
+  _ChangingValueState createState() => _ChangingValueState();
+}
+
+class _ChangingValueState extends State<ChangingValue> {
+  late int _currentValue;
+
+  @override
+  void initState() {
+    _currentValue = 50;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          iconSize: 50,
+          icon: const Icon(Icons.add_circle),
+          onPressed: () {
+            setState(() {
+              _currentValue += 1;
+            });
+          },
+        ),
+        const SizedBox(width: 50),
+        Text(
+          "$_currentValue",
+          style: TextStyle(
+            fontSize: 50,
+            fontWeight: FontWeight.w900,
+            color: Colors.indigo,
+          ),
+        ),
+        const SizedBox(width: 50),
+        IconButton(
+          iconSize: 50,
+          icon: const Icon(Icons.remove_circle),
+          onPressed: () {
+            setState(() {
+              _currentValue -= 1;
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
